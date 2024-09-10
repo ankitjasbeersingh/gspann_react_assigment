ad1398151b44609fc738e861512888438c493402
Ankit Singh <ankitjasbeersingh@gmail.com>
2024-09-10 17:05:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchProducts = async () => {
+    const res = await fetch('https://dummyjson.com/products?limit=100');
+    const data = await res.json();
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
+
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    ) {
+      setPage(selectedPage);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="products">
+        {products.slice(page * 10 - 10, page * 10).map((product) => {
+          return (
+            <span className="products__single" key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <span>{product.title}</span>
+            </span>
+          );
+        })}
+      </div>
+      {products.length > 0 && (
+        <div className="pagination">
+          <span onClick={() => selectPageHandler(page - 1)}>Prev</span>
+          {[...Array(products.length / 10)].map((__, i) => {
+            return (
+              <span
+                className={page === i + 1 ? 'pagination__selected' : ''}
+                onClick={() => selectPageHandler(i + 1)}
+                key={i}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+
+          <span onClick={() => selectPageHandler(page + 1)}>Next</span>
+        </div>
+      )}
     </div>
   );
 }
